7b12ba51aa7a6b42bae1b4e65d4494fb9848cadc
314159otr <piotrbpersonal@gmail.com>
2026-05-04 21:58:51 +0200

    CH4: Blocks - L2: Block Types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,4 @@
+from enum import Enum
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -7,3 +8,47 @@ def markdown_to_blocks(markdown):
             continue
         result_blocks.append(block.strip())
     return result_blocks
+
+class BlockType(Enum):
+    paragraph = 1
+    heading = 2
+    code = 3
+    quote = 4
+    unordered_list = 5
+    ordered_list = 6
+
+def block_to_block_type(block):
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.heading
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.code
+    if block.startswith(">"):
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.paragraph
+        return BlockType.quote
+    if block.startswith("- "):
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.paragraph
+        return BlockType.unordered_list
+    n = None
+    for chars in range(0, len(block)):
+        if block[0:chars+1].isdigit():
+            continue
+        if chars > 0:
+            n = block[0:chars]
+        break
+    if n is not None and block.startswith(". ", len(n)):
+        n = int(n)
+        lines = block.split("\n")
+        for line in lines:
+            if not line.startswith(str(n) + ". "):
+                return BlockType.paragraph
+            n += 1
+        return BlockType.ordered_list
+
+    return BlockType.paragraph
+
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -44,3 +44,54 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_paragraph(self):
+        block = "normal paragraph"
+        result = block_to_block_type(block)
+        expected = BlockType.paragraph
+        self.assertEqual(result, expected)
+
+    def test_block_to_heading(self):
+        block = "# heading"
+        result = block_to_block_type(block)
+        expected = BlockType.heading
+        self.assertEqual(result, expected)
+
+    def test_block_to_code(self):
+        block = "```\ncode block```"
+        result = block_to_block_type(block)
+        expected = BlockType.code
+        self.assertEqual(result, expected)
+        
+    def test_block_to_quote(self):
+        block = "> first quote line\n> second quote line"
+        result = block_to_block_type(block)
+        expected = BlockType.quote
+        self.assertEqual(result, expected)
+        
+    def test_block_to_unordered_list(self):
+        block = "- first item\n- second item"
+        result = block_to_block_type(block)
+        expected = BlockType.unordered_list
+        self.assertEqual(result, expected)
+        
+    def test_block_to_ordered_list(self):
+        block = "1. first item\n2. second item"
+        result = block_to_block_type(block)
+        expected = BlockType.ordered_list
+        self.assertEqual(result, expected)
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.quote)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.unordered_list)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ordered_list)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.paragraph)
+
